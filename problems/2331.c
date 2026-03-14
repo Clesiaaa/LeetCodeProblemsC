b1835bc2e6e4839c00b2ec559d1bcd289ff458b2
Clesiaaa <robert.folga@outlook.com>
2026-03-14 22:33:24 +0100

    fix : solution for problem 2331

---
+++ problems/2331.c
@@ -0,0 +1,8 @@
+bool evaluateTree(struct TreeNode* root) {
+    if (root == NULL) return false;
+    if (root->val == 0) return false;
+    if (root->val == 1) return true;
+    if (root->val == 2) return evaluateTree(root->left) || evaluateTree(root->right);
+    if (root->val == 3) return evaluateTree(root->left) && evaluateTree(root->right);
+    return false;
+}
