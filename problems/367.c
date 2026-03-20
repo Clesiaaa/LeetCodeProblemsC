8f7d5b15ea662b4cad000ae0e021e0d597d4a862
Clesiaaa <robert.folga@outlook.com>
2026-03-20 23:31:54 +0100

    enhancement : 367

--- problems/367.c
+++ problems/367.c
@@ -1,7 +1,4 @@
 bool isPerfectSquare(int num)
 {
-    for (long i = 1; i * i <= num; i++)
-        if (i * i == num)
-            return true;
-    return false;
+    return (int)sqrt((double)num) * (int)sqrt((double)num) == num ? true : false;
 }
